16c4007d1659825f47c7f24bfb04f96bc75b7efe
nicolas <nicolas@nicolas-ad21>
2018-11-17 18:28:36 +0100

    ajout des coupes lon NCL

--- tools/PFTools.js
+++ tools/PFTools.js
@@ -48,6 +48,15 @@ module.exports.createMaps = function(context)
     context.europe_dir = path.join(context.config.PRODUCT_DIR, "maps/europe/pifo", path.basename(context.productDir));
     Helper.mkdirpSync(context.europe_dir);
     Helper.createSymlink(context.europe_dir, path.join(context.config.PRODUCT_DIR, "maps/europe/pifo", "latest"));
+       
+    context.france_dir = path.join(context.config.PRODUCT_DIR, "maps/france/pifo/"+context.product);
+    Helper.mkdirpSync(context.france_dir);
+
+    context.meteogrammes_dir = path.join(context.config.PRODUCT_DIR, "charts/france/pifo/"+context.product);
+    Helper.mkdirpSync(context.meteogrammes_dir);
+    
+    context.coupes_dir = path.join(context.config.PRODUCT_DIR, "slices/france/pifo/"+context.product);
+    Helper.mkdirpSync(context.coupes_dir);
     
     execSync('/bin/cp '+ path.join(context.config.products[context.productName].pifo_root, "output", "fileinfo.txt")+" "+context.europe_dir)
     
@@ -55,7 +64,10 @@ module.exports.createMaps = function(context)
     var args = [
         path.join(context.config.products[context.productName].custom_root, "pifo_all.ncl"),
         "input_dir=\""+path.join(context.config.products[context.productName].pifo_root, "output")+"\"",
-        "europe_dir=\""+context.europe_dir+"\""
+        "europe_dir=\""+context.europe_dir+"\"",
+        "france_dir=\""+context.france_dir+"\"",
+        "meteogram_dir=\""+context.meteogrammes_dir+"\"",
+        "coupes_dir=\""+context.coupes_dir+"\""
         ];
     return Helper.spawnPromise(cmdline, args, context);
 }
@@ -64,6 +76,36 @@ module.exports.createImagesEurope = function(context)
 {
     var cmdline = path.join(context.config.products[context.productName].script_root, "createimage_europe.sh");
     return Helper.dirAndProcess(context, context.europe_dir+"/*.ps", 
+        (file) => {
+            return Helper.spawnPromise(cmdline, [file], context);
+        });
+}
+
+module.exports.createImagesFrance = function(context)
+{
+    var filenum = context.file.substring(context.file.length-3, context.file.length);
+    var cmdline = path.join(context.config.products[context.productName].script_root,"createimage_france.sh");
+    return Helper.dirAndProcess(context, context.france_dir+"/*_"+filenum+".ps", 
+        (file) => {
+            return Helper.spawnPromise(cmdline, [file], context);
+        });
+}
+
+module.exports.createSkewt = function(context)
+{
+    var filenum = context.file.substring(context.file.length-3, context.file.length);
+    var cmdline = path.join(context.config.products[context.productName].script_root,"createimage_skewt.sh");
+    return Helper.dirAndProcess(context, context.meteogrammes_dir+"/*_"+filenum+".ps", 
+        (file) => {
+            return Helper.spawnPromise(cmdline, [file], context);
+        });
+}
+
+module.exports.createSlices = function(context)
+{
+    var filenum = context.file.substring(context.file.length-3, context.file.length);
+    var cmdline = path.join(context.config.products[context.productName].script_root,"createimage_slices.sh");
+    return Helper.dirAndProcess(context, context.coupes_dir+"/*_"+filenum+".ps", 
         (file) => {
             return Helper.spawnPromise(cmdline, [file], context);
         });
